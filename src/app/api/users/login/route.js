a926bc4b77dbf4d990d050ef2faca4bfdbcb539b
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-03-24 17:38:10 +0530

    login

---
+++ src/app/api/users/login/route.js
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+import User from "@/models/userModel";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const createToken = (_id) => {
+  try {
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+  } catch (error) {
+    console.error("Error creating token:", error);
+    return null;
+  }
+};
+
+export async function POST(req) {
+  const body = await req.json();
+  const { email, password } = body;
+  try {
+    if (!email || !password) {
+      throw new Error("All field must be filled");
+    }
+
+    const user = await User.findOne({ email: email.toLowerCase() });
+    if (!user) {
+      throw new Error("Email not found");
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      throw new Error("Incorrect password");
+    }
+    const role = user.isAdmin;
+    const token = createToken(user._id);
+    return NextResponse.json(
+      {
+        email,
+        username: user.username,
+        token,
+        isAdmin,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 400 });
+    } else {
+      return NextResponse.json({ mesage: "error", error }, { status: 500 });
+    }
+  }
+}
