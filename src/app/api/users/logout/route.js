4f460b062d57244ec3e8a400fb962902fd5c8a73
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-03-29 21:03:32 +0530

    logout

---
+++ src/app/api/users/logout/route.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const response = NextResponse.json(
+      {
+        message: "logout successful",
+      },
+      { status: 200 }
+    );
+    response.cookies.set("token", "", { httpOnly: true, secure: true });
+    response.cookies.set("user", "", { httpOnly: false, secure: true });
+    return response;
+  } catch (error) {
+    return NextResponse.json({ message: "Unable to logout" }, { status: 400 });
+  }
+}
