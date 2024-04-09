af935067b8539df1499835cbe66d639898723238
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-04-09 23:22:39 +0530

    daiiy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,7 +14,24 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        cardBg: "white",
+        cardBtn: "#black",
+      },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          // importing the built-in 'light' theme
+          // and setting the color values for '--primary-muted'
+          // (numbers are HSL values)
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          "--primary-muted": "338 83% 66%",
+        },
+      },
+    ],
+  },
+  plugins: [daisyui],
 };
