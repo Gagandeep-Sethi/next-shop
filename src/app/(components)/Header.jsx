3a1a136a5399c195329799feff7cd3511ba035e1
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-04-10 23:31:03 +0530

    nav bar

--- src/app/(components)/Header.jsx
+++ src/app/(components)/Header.jsx
@@ -5,13 +5,16 @@ import SubNav from "./SubNav";
 import Sidebar from "./Sidebar";
 import { useSelector } from "react-redux";
 import { IoHome } from "react-icons/io5";
+import Link from "next/link";
 
 const Header = () => {
   const sidebar = useSelector((store) => store?.sidebar);
   return (
     <div className="flex justify-between shadow-lg sticky top-0 z-[20] w-full bg-white">
       <div className="p-4 flex items-center">
-        <IoHome className="w-7 h-7  text-black transition-colors duration-300 hover:text-orange-500" />
+        <Link href="/">
+          <IoHome className="w-7 h-7  text-black transition-colors duration-300 hover:text-orange-500" />
+        </Link>
 
         <div className=" lg:">
           <NavLinks />
@@ -19,7 +22,7 @@ const Header = () => {
       </div>
       <SubNav />
       {sidebar ? (
-        <div className="w-screen absolute z-30  top-0 left-0 bottom-0 bg-white  ">
+        <div className="w-screen absolute z-30  top-0 left-0 bottom-0 min-   ">
           <Sidebar />
         </div>
       ) : null}
---
+++ src/app/(components)/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Image from "next/image";
+import { AddsHome } from "./AddsHome";
+const Home = () => {
+  return <div className="   flex justify-center ">home</div>;
+};
+
+export default Home;
