4f57ca2fe1278d5d2bcb35fa056bdc9b855394f4
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-04-12 12:47:56 +0530

    header

--- src/app/(components)/Header.jsx
+++ src/app/(components)/Header.jsx
@@ -1,33 +1,114 @@
-"use client";
-import React from "react";
-import NavLinks from "./NavLinks";
-import SubNav from "./SubNav";
-import Sidebar from "./Sidebar";
-import { useSelector } from "react-redux";
+import { RxHamburgerMenu } from "react-icons/rx";
+
 import { IoHome } from "react-icons/io5";
+import { IoSearchOutline } from "react-icons/io5";
+import { CiShoppingCart, CiUser } from "react-icons/ci";
 import Link from "next/link";
 
+import NavLinks from "./NavLinks";
 const Header = () => {
-  const sidebar = useSelector((store) => store?.sidebar);
   return (
-    <div className="flex justify-between shadow-lg sticky top-0 z-[20] w-full bg-white">
-      <div className="p-4 flex items-center">
-        <Link href="/">
-          <IoHome className="w-7 h-7  text-black transition-colors duration-300 hover:text-orange-500" />
-        </Link>
+    <div className="drawer-end ">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col">
+        {/* Navbar */}
+        <div className="w-full flex justify-between navbar bg-base-300">
+          <div className="flex space-x-5">
+            <div>
+              <Link href="/">
+                <IoHome className="w-7 h-7  text-black transition-colors duration-300 hover:text-orange-500" />
+              </Link>
+            </div>
+            <NavLinks />
+          </div>
 
-        <div className=" lg:">
-          <NavLinks />
+          <div className="flex space-x-2 lg:space-x-4">
+            <div className=" ">
+              <Link href="/search">
+                <IoSearchOutline className="w-7 h-6  text-black transition-colors duration-300 hover:text-orange-500 " />
+              </Link>
+            </div>
+            <div className=" ">
+              <Link href="/cart">
+                <CiShoppingCart className="w-7 h-7  text-black transition-colors duration-300 hover:text-orange-500" />
+              </Link>
+            </div>
+            <div className="hidden md:flex ">
+              <Link href="/user">
+                <CiUser className="w-7 h-7  text-black transition-colors duration-300 hover:text-orange-500" />
+              </Link>
+            </div>
+            <div className="flex-none lg:hidden">
+              <label
+                htmlFor="my-drawer-3"
+                aria-label="open sidebar"
+                className="btn btn-square btn-ghost"
+              >
+                <RxHamburgerMenu className="w-7 h-6" />
+              </label>
+            </div>
+          </div>
         </div>
       </div>
-      <SubNav />
-      {sidebar ? (
-        <div className="w-screen absolute z-30  top-0 left-0 bottom-0 min-   ">
-          <Sidebar />
-        </div>
-      ) : null}
+
+      <div className="drawer-side z-20">
+        <label
+          htmlFor="my-drawer-3"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+
+        <ul className="menu p-4 w-80 min-h-full bg-base-200 space-y-">
+          {/* Sidebar content here */}
+          <Link href="/user/login">
+            <h1 className="flex items-center">
+              <CiUser className="m-2 w-6 h-6 text-black" />
+              SignUp/SignIn
+            </h1>
+          </Link>
+          <li aria-label="close sidebar">
+            <Link
+              href="/"
+              className="mr-4 transition-colors duration-300 hover:text-orange-500"
+            >
+              Cushion
+            </Link>
+          </li>
+          <li aria-label="close sidebar">
+            <Link
+              href="/"
+              className="mr-4 transition-colors duration-300 hover:text-orange-500"
+            >
+              Pillow
+            </Link>
+          </li>
+          <li aria-label="close sidebar">
+            <Link
+              href="/"
+              className="mr-4 transition-colors duration-300 hover:text-orange-500"
+            >
+              Mattress
+            </Link>
+          </li>
+          <li aria-label="close sidebar">
+            <Link
+              href="/"
+              className="mr-4 transition-colors duration-300 hover:text-orange-500"
+            >
+              Bolster
+            </Link>
+          </li>
+          <li aria-label="close sidebar">
+            <Link
+              href="/"
+              className="mr-4 transition-colors duration-300 hover:text-orange-500"
+            >
+              Customize product
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
-
 export default Header;
