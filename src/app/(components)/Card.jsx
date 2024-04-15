d41583ba07b30fd0aeba591c27acac4e58c0d643
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-04-15 17:48:23 +0530

    card

--- src/app/(components)/Card.jsx
+++ src/app/(components)/Card.jsx
@@ -7,7 +7,6 @@ import PopUpMessage from "./PopUpMessage";
 import Link from "next/link";
 
 const Card = ({ data }) => {
-  console.log(data, "data");
   const dispatch = useDispatch();
   const handleCart = () => {
     dispatch(addToCart(data));
@@ -17,35 +16,37 @@ const Card = ({ data }) => {
   const [showPopup, setShowPopup] = useState(false);
   const [popupMessage, setPopupMessage] = useState("");
   return (
-    <Link href={`/product/${data._id}`}>
-      <div className="card card-compact w-80 bg-gray-200  shadow-xl">
+    <div className="card card-compact w-80 bg-gray-200  shadow-xl">
+      <Link href={`/product/${data._id}`}>
         <figure>
           <Image
             src={`https://res.cloudinary.com/dyja4tbmu/${data?.images[0]}.jpg`}
             width={320}
             height={320}
             alt="Shoes"
+            className="rounded-t-2xl"
           />
         </figure>
-        <div className="card-body ">
-          <h2 className="card-title text-gray-800">{data?.name}</h2>
-          <p className="text-gray-600">
-            ₹{data?.displayPrice || data?.originalPrice}
-          </p>
-          <div className="card-actions justify-end">
-            <button
-              onClick={handleCart}
-              className="btn bg-black hover:bg-primary text-white"
-            >
-              Add to Cart
-            </button>
-          </div>
+      </Link>
+
+      <div className="card-body ">
+        <h2 className="card-title text-gray-800">{data?.name}</h2>
+        <p className="text-gray-600">
+          ₹{data?.displayPrice || data?.originalPrice}
+        </p>
+        <div className="card-actions justify-end">
+          <button
+            onClick={handleCart}
+            className="btn bg-black hover:bg-primary text-white"
+          >
+            Add to Cart
+          </button>
         </div>
-        {showPopup && (
-          <PopUpMessage message={popupMessage} setShowPopup={setShowPopup} />
-        )}
       </div>
-    </Link>
+      {showPopup && (
+        <PopUpMessage message={popupMessage} setShowPopup={setShowPopup} />
+      )}
+    </div>
   );
 };
 
--- src/app/(components)/CardContainer.jsx
+++ src/app/(components)/CardContainer.jsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 const CardContainer = ({ data }) => {
   return (
     <div className="">
-      <div className="grid lg:grid-flow-col lg:col-span-3 justify-evenly">
+      <div className=" flex flex-wrap justify-evenly gap-4">
         {data
           ? data.map((res) => (
               <div className="mt-7 md:mt-0  " key={res._id}>
@@ -13,16 +13,9 @@ const CardContainer = ({ data }) => {
             ))
           : null}
       </div>
-      <div className="flex justify-center my-6">
-        <button
-          onClick=""
-          className="btn btn-xs sm:btn-sm md:btn-md bg-black hover:bg-primary text-white "
-        >
-          See more
-        </button>
-      </div>
     </div>
   );
 };
 
 export default CardContainer;
+//grid lg:grid-flow-col lg:col-span-3 justify-evenly
