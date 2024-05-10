0aa1a140e061f4117d4198390c6d5fe14451f550
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-11 00:00:06 +0530

    home page

--- src/app/(components)/Home.jsx
+++ src/app/(components)/Home.jsx
@@ -2,10 +2,11 @@
 import React from "react";
 import useAllProducts from "../(hooks)/useAllProducts";
 import { useSelector } from "react-redux";
-import CardContainer from "./CardContainer";
 import Link from "next/link";
 import Skeleton from "./Skeleton";
 import TestCarousel from "./TestCorousal";
+import HomeCardContainer from "./HomeCardContainer";
+import { FaArrowRight } from "react-icons/fa";
 
 const Home = () => {
   const products = useSelector((store) => store?.product);
@@ -13,7 +14,7 @@ const Home = () => {
   console.log(products, "products");
 
   return (
-    <div className="min-h-svh    ">
+    <div className="min-h-svh  bg-gray-50  ">
       <div className="w-11/12 md:h-[740px] h-[500px] mx-auto ">
         {products?.bolster !== null &&
         products?.bolster[0]?.images.length > 0 ? (
@@ -22,66 +23,66 @@ const Home = () => {
           <div className="skeleton w-11/12 md:h-[600px] h-[300px] mx-auto  mt-6 "></div>
         )}
       </div>
-      <h1 className="my-6 text-3xl text-center underline">Pillows</h1>
-      <div>
+      <h1 className="mt-6 mb-2 text-4xl text-center    font-space">Pillows</h1>
+      <div className=" w-full ">
         {products?.pillow ? (
-          <CardContainer data={products?.pillow.slice(0, 3)} />
+          <HomeCardContainer data={products?.pillow.slice(0, 4)} />
         ) : (
-          <Skeleton count={3} />
+          <Skeleton count={4} />
         )}
 
         <div className="flex justify-center my-6 md:my-10">
           <Link href="/product/category/pillow">
             <button className="btn btn-xs sm:btn-sm md:btn-md bg-black hover:bg-primary text-white ">
-              See more
+              See more <FaArrowRight />
             </button>
           </Link>
         </div>
       </div>
 
-      <h1 className="my-6 text-3xl text-center underline">Cushions</h1>
+      <h1 className="mt-6 mb-2 text-3xl text-center  font-space">Cushions</h1>
       <div>
         {products?.cushion ? (
-          <CardContainer data={products?.cushion.slice(0, 3)} />
+          <HomeCardContainer data={products?.cushion.slice(0, 4)} />
         ) : (
-          <Skeleton count={3} />
+          <Skeleton count={4} />
         )}
         <div className="flex justify-center my-6">
           <Link href="/product/category/cushion">
             <button className="btn btn-xs sm:btn-sm md:btn-md bg-black hover:bg-primary text-white ">
-              See more
+              See more <FaArrowRight />
             </button>
           </Link>
         </div>
       </div>
 
-      <h1 className="my-6 text-3xl text-center underline">Bolsters</h1>
+      <h1 className="mt-6 mb-2 text-3xl text-center  font-space">Bolsters</h1>
       <div>
         {products?.bolster ? (
-          <CardContainer data={products?.bolster} />
+          <HomeCardContainer data={products?.bolster.slice(0, 4)} />
         ) : (
-          <Skeleton count={3} />
+          <Skeleton count={4} />
         )}
         <div className="flex justify-center my-6">
           <Link href="/product/category/bolster">
             <button className="btn btn-xs sm:btn-sm md:btn-md bg-black hover:bg-primary text-white ">
-              See more
+              See more <FaArrowRight />
             </button>
           </Link>
         </div>
       </div>
 
-      <h1 className="my-6 text-3xl text-center underline">Mattresses</h1>
+      <h1 className="mt-6 mb-2 text-3xl text-center  font-space">Mattresses</h1>
       <div>
         {products?.mattress ? (
-          <CardContainer data={products?.mattress} />
+          <HomeCardContainer data={products?.mattress.slice(0, 4)} />
         ) : (
-          <Skeleton count={3} />
+          <Skeleton count={4} />
         )}
         <div className="flex justify-center my-6">
           <Link href="/product/category/mattress">
             <button className="btn btn-xs sm:btn-sm md:btn-md bg-black hover:bg-primary text-white ">
-              See more
+              See more <FaArrowRight />
             </button>
           </Link>
         </div>
--- src/app/(hooks)/useAllProducts.js
+++ src/app/(hooks)/useAllProducts.js
@@ -18,60 +18,34 @@ const useAllProducts = () => {
     getData();
     async function getData() {
       try {
-        // const data = await fetch(`/api/product`);
-        // const json = await data.json();
         if (products?.pillow === null) {
-          console.log(products?.pillow, "pillow");
           const pillow = await fetch(
-            `/api/product/category?type=pillow&limit=3`
+            `/api/product/category?type=pillow&limit=4`
           );
           const pillows = await pillow.json();
           dispatch(addPillows(pillows?.product));
         }
         if (products?.bolster === null) {
-          console.log(products?.bolster, "bolster");
           const bolster = await fetch(
-            `/api/product/category?type=bolster&limit=3`
+            `/api/product/category?type=bolster&limit=4`
           );
           const bolsters = await bolster.json();
           dispatch(addBolsters(bolsters?.product));
         }
         if (products?.cushion === null) {
-          console.log(products?.cushion, "cushion");
-
           const cushion = await fetch(
-            `/api/product/category?type=cushion&limit=3`
+            `/api/product/category?type=cushion&limit=4`
           );
           const cushions = await cushion.json();
           dispatch(addCushions(cushions?.product));
         }
         if (products?.mattress === null) {
-          console.log(products?.mattress, "mattress");
-
-          console.log("mattress called");
           const mattress = await fetch(
-            `/api/product/category?type=mattress&limit=3`
+            `/api/product/category?type=mattress&limit=4`
           );
           const mattresses = await mattress.json();
           dispatch(addMattresses(mattresses?.product));
         }
-        // console.log(pillows, "pillow");
-        // console.log(bolsters, "bolster");
-        // console.log(cushions, "cushions");
-        // console.log(mattresses, "mattress");
-
-        // const pillows = json?.product.filter(
-        //   (res) => res.category === "pillow"
-        // );
-        // const bolsters = json?.product.filter(
-        //   (res) => res.category === "bolster"
-        // );
-        // const cushions = json?.product.filter(
-        //   (res) => res.category === "cushion"
-        // );
-        // const mattresses = json?.product.filter(
-        //   (res) => res.category === "mattress"
-        // );
       } catch (error) {
         console.log(error);
       }
