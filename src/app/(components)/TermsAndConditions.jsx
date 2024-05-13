ae7cd1807bfad453f847c511a7819eaa522795a8
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-13 21:25:25 +0530

    terms and condition

---
+++ src/app/(components)/TermsAndConditions.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+const TermsAndConditions = () => {
+  return (
+    <div className="md:px-20 px-10 min-h-svh">
+      <div className="text-left ">
+        <h1 className="text-2xl   font-space py-4 md:py-6">
+          Terms and Conditions
+        </h1>
+        <p className="text-gray-400">
+          Welcome to Sky-Shop! These terms and conditions outline the rules and
+          regulations for the use of the Sky-Shop website, located at [Website
+          URL]. By accessing this website, we assume you accept these terms and
+          conditions. Do not continue to use Sky-Shop if you do not agree to
+          take all of the terms and conditions stated on this page.
+        </p>
+
+        <h1 className="text-2xl   font-space py-4 md:py-6">Cookies</h1>
+        <p className="text-gray-400">
+          We employ the use of cookies. By accessing Sky-Shop, you agree to use
+          cookies in agreement with the Sky-Shop Privacy Policy.
+        </p>
+
+        <h1 className="text-2xl   font-space py-4 md:py-6">
+          Intellectual Property Rights
+        </h1>
+        <p className="text-gray-400">
+          Unless otherwise stated, Sky-Shop and/or its licensors own the
+          intellectual property rights for all material on Sky-Shop. All
+          intellectual property rights are reserved. You may access this from
+          Sky-Shop for your own personal use subjected to restrictions set in
+          these terms and conditions.
+        </p>
+
+        <h1 className="text-2xl   font-space py-4 md:py-6">Comments</h1>
+        <p className="text-gray-400">
+          Parts of this website offer an opportunity for users to post and
+          exchange opinions and information in certain areas of the website.
+          Sky-Shop does not filter, edit, publish or review Comments prior to
+          their presence on the website. Comments do not reflect the views and
+          opinions of Sky-Shop, its agents, and/or affiliates. Comments reflect
+          the views and opinions of the person who posts their views and
+          opinions.
+        </p>
+
+        <h1 className="text-2xl   font-space py-4 md:py-6">Liability</h1>
+        <p className="text-gray-400">
+          Sky-Shop shall not be liable for any comments or for any liability,
+          damages, or expenses caused and/or suffered as a result of any use of
+          and/or posting of and/or appearance of the Comments on this website.
+        </p>
+
+        <h1 className="text-2xl   font-space py-4 md:py-6">
+          Modification of Terms
+        </h1>
+        <p className="text-gray-400">
+          Sky-Shop reserves the right to modify these terms and conditions at
+          any time. By using this website, you are agreeing to be bound by the
+          current version of these terms and conditions.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TermsAndConditions;
