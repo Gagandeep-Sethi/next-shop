7111bca64fc6c0ddb7c100e7fec6eb4d8ea60ef2
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-13 21:27:46 +0530

    popup updates

--- src/app/(components)/PopUpMessage.jsx
+++ src/app/(components)/PopUpMessage.jsx
@@ -1,8 +1,9 @@
 "use client";
 import React, { useEffect } from "react";
 import { FiCheckCircle } from "react-icons/fi";
+import { MdError } from "react-icons/md";
 
-const PopUpMessage = ({ message, setShowPopup, timeout = 2000 }) => {
+const PopUpMessage = ({ message, status, setShowPopup, timeout = 2000 }) => {
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       setShowPopup(false);
@@ -12,10 +13,21 @@ const PopUpMessage = ({ message, setShowPopup, timeout = 2000 }) => {
   }, [setShowPopup, timeout]);
 
   return (
-    <div role="alert" className="alert alert-success">
-      <FiCheckCircle />
+    <div>
+      {status === "success" ? (
+        <div role="alert" className="alert alert-success">
+          <FiCheckCircle />
 
-      <span>{message}</span>
+          <span>{message}</span>
+        </div>
+      ) : null}
+      {status === "fail" ? (
+        <div role="alert" className="alert alert-error">
+          <MdError />
+
+          <span>{message}</span>
+        </div>
+      ) : null}
     </div>
   );
 };
