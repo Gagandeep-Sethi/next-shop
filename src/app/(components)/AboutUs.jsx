9899b298105a335103ada60083b9c80b184017b8
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-13 21:25:01 +0530

    about us page

---
+++ src/app/(components)/AboutUs.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="md:px-20 px-10 min-h-svh">
+      <div className="text-center ">
+        <h1 className="text-2xl   font-space py-4 md:py-6">About Us</h1>
+        <p className="text-gray-400">
+          Welcome to Sky-Shop, your one-stop destination for premium pillows,
+          cushions, mattresses, and bolsters! At Sky-Shop, we are passionate
+          about providing our customers with the highest quality products that
+          enhance their comfort and style. Our carefully curated collection is
+          designed to elevate your living space and transform your everyday
+          routines into luxurious experiences. We understand that everyone has
+          unique preferences when it comes to comfort, which is why we offer a
+          diverse range of products to suit different needs and preferences.
+          Whether you are looking for a soft and plush pillow, a supportive
+          bolster, or a cozy mattress, we have you covered. Quality is at the
+          heart of everything we do. Each product in our collection is crafted
+          with attention to detail and made from premium materials to ensure
+          durability and comfort. We believe that everyone deserves to enjoy a
+          good night sleep and relax in style, which is why we strive to offer
+          our products at affordable prices. Our commitment to customer
+          satisfaction drives us to continually improve and innovate. We are
+          dedicated to providing exceptional customer service and are always
+          here to assist you with any questions or concerns you may have. Thank
+          you for choosing Sky-Shop. We look forward to helping you create a
+          more comfortable and stylish home.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
