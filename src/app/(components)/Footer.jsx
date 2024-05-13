65fc52e27c37c6e42115a223cb3a9829e3d8aa56
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-13 21:25:45 +0530

    footer

--- src/app/(components)/Footer.jsx
+++ src/app/(components)/Footer.jsx
@@ -1,41 +1,45 @@
 import Link from "next/link";
 import React from "react";
-import { AiFillInstagram } from "react-icons/ai";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
 
 const Footer = () => {
   return (
     <div>
       <footer className="footer footer-center p-10 bg-gray-300 text-base-content rounded">
         <nav className="grid grid-flow-col gap-4">
-          <Link href="/" className="link link-hover">
+          <Link href="/aboutus" className="link link-hover">
             About us
           </Link>
-          <Link href="/" className="link link-hover">
+          <Link
+            href="mailto:gagandeepsethi.7895@gmail.com"
+            className="link link-hover"
+          >
             Contact
           </Link>
-          <Link href="/" className="link link-hover">
-            Jobs
-          </Link>
-          <Link href="/" className="link link-hover">
-            Press kit
+          <Link href="/termsandconditions" className="link link-hover">
+            Terms & Conditions
           </Link>
         </nav>
         <nav>
           <div className="grid grid-flow-col gap-4">
-            <Link href="/">
-              <AiFillInstagram className="w-8 h-8" />
+            <Link
+              href="https://www.linkedin.com/in/gagandeep-sethi-560796206/"
+              target="_blank"
+            >
+              <FaLinkedin className="w-8 h-8" />
             </Link>
 
-            <Link href="/">
-              <AiFillInstagram className="w-8 h-8" />
+            <Link href="https://github.com/gagandeep-sethi" target="_blank">
+              <FaGithub className="w-8 h-8" />
             </Link>
-            <Link href="/">
-              <AiFillInstagram className="w-8 h-8" />
+            <Link href="mailto:gagandeepsethi.7895@gmail.com">
+              <SiGmail className="w-8 h-8" />
             </Link>
           </div>
         </nav>
         <aside>
-          <p>Copyright © 2024 - All right reserved by ABCD Industries Ltd</p>
+          <p>Copyright © 2024 - All right reserved by SKY Industries Ltd</p>
         </aside>
       </footer>
     </div>
