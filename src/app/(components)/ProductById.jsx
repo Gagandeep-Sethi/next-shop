d51482fa0456c305f5efdf38ec3dae825b2a2ea7
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-15 00:06:35 +0530

    ui fix

--- src/app/(components)/ProductById.jsx
+++ src/app/(components)/ProductById.jsx
@@ -8,47 +8,79 @@ import Skeleton from "./Skeleton";
 import Link from "next/link";
 import Review from "./Review";
 import RevewList from "./RevewList";
+import Image from "next/image";
 
 const ProductById = () => {
   const params = useParams();
   const [product, setProduct] = useState(null);
   const [showPopup, setShowPopup] = useState(false);
   const [popupMessage, setPopupMessage] = useState("");
+  const [status, setStatus] = useState("");
   const dispatch = useDispatch();
   const user = useSelector((store) => store?.user?.user);
 
   const { id } = params;
-  console.log(id);
   const handleCart = () => {
     dispatch(addToCart(product));
     setPopupMessage("Item added to cart");
     setShowPopup(true);
+    setStatus("success");
   };
+
+  async function handleDelete() {
+    const response = await fetch(`/api/product/deleteProduct/${id}`);
+    if (response.ok) {
+      setProduct("product not found");
+    } else {
+      setPopupMessage("failed to delete product please try again");
+      setShowPopup(true);
+      setStatus("fail");
+    }
+  }
 
   useEffect(() => {
     getData();
     async function getData() {
       try {
         const data = await fetch(`/api/product/${id}`);
-
-        if (!data.ok) {
-          throw new Error("Failed to fetch product");
-        }
-
         const json = await data.json();
-        console.log(json?.product);
-        setProduct(json?.product);
+        if (!data.ok) {
+          setProduct(json.message);
+        } else {
+          setProduct(json?.product);
+        }
       } catch (error) {
         console.log(error);
       }
     }
   }, [id]);
-  if (!product)
+
+  if (product === "product not found") {
+    return (
+      <div className="flex flex-col justify-center min-h-svh  ">
+        <p className="text-center  text-4xl text-blue-600 ">
+          Oops.. product details not found !!
+        </p>
+        <div className="flex justify-center">
+          <Image src="/search.png" alt="" width={320} height={500} />
+        </div>
+        <div className="flex justify-center mt-10">
+          <Link href="/">
+            <button className="  btn btn-sm md:btn-md lg:btn-lg bg-black text-white hover:bg-gray-700">
+              HomePage
+            </button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+  if (!product) {
     return (
       <div>
         <Skeleton count={6} />
       </div>
     );
+  }
   return (
     <div className="w-screen font-">
       <div className="w-full items-center md:flex whitespace-normal break-words ">
@@ -71,7 +103,9 @@ const ProductById = () => {
             <p className="py-3 md:py-6">₹{product?.originalPrice}</p>
           )}
           {product?.isOutOfStock === "true" ? (
-            <p className="text-red-500">Currently Out of Stock !!</p>
+            <p className="text-red-500 text-center md:text-start my-2">
+              Currently Out of Stock !!
+            </p>
           ) : null}
           <div className="flex  justify-center md:justify-start">
             <button
@@ -88,25 +122,64 @@ const ProductById = () => {
               className="w-96"
               message={popupMessage}
               setShowPopup={setShowPopup}
+              status={status}
             />
           )}
 
           {user?.isAdmin ? (
-            <div className="mt-10 flex justify-center md:justify-start">
-              <Link href={`/product/updateproduct/${id}`}>
-                <button className="btn bg-red-700 hover:bg-primary text-white ">
-                  Update Product
+            <div className="mt-4 flex justify-center md:justify-start">
+              <div className="flex justify-center">
+                <button
+                  className="btn bg-red-500 hover:bg-red-700 text-white  "
+                  onClick={() =>
+                    document.getElementById("my_modal_1").showModal()
+                  }
+                >
+                  Delete Product
                 </button>
-              </Link>
+
+                <dialog id="my_modal_1" className="modal">
+                  <div className="modal-box">
+                    <p className="py-4">
+                      Are you sure you want to delete product !!
+                    </p>
+                    <div className="flex justify-center items-center">
+                      <div>
+                        <button
+                          onClick={handleDelete}
+                          className="btn bg-green-500 hover:bg-green-600 text-white  btn-sm md:btn-md "
+                        >
+                          Yes
+                        </button>
+                      </div>
+                      <div className="modal-action justify-center mt-0">
+                        <form method="dialog">
+                          {/* if there is a button in form, it will close the modal */}
+                          <button className="bg-red-500 hover:bg-red-600 btn text-white  btn-sm md:btn-md ">
+                            No
+                          </button>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                  <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                  </form>
+                </dialog>
+              </div>
+              <div>
+                <Link href={`/product/updateproduct/${id}`}>
+                  <button className="btn ml-4 bg-red-500 hover:bg-red-700 text-white ">
+                    Update Product
+                  </button>
+                </Link>
+              </div>
             </div>
           ) : null}
         </div>
       </div>
-      <div className="  ">
+      <div className="my-6  ">
         <RevewList id={id} />
-      </div>
-      <div className="flex justify-center py-6">
-        <Review email={user?.email} id={id} />
       </div>
     </div>
   );
--- src/app/(components)/Rating.jsx
+++ src/app/(components)/Rating.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ReviewCard from "./ReviewCard";
 
-const Rating = () => {
-  return <div>Rating</div>;
+const Rating = ({ email }) => {
+  const [productBought, setProductBought] = useState(null);
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(`/api/product/bought/${email}`);
+      const json = await response.json();
+      console.log(json, "json");
+
+      setProductBought(json);
+    }
+    getData();
+  }, [email]);
+  if (productBought === null || productBought === "An error occurred")
+    return <div>No products bought</div>;
+  else console.log(productBought?.productBought[0]?.productBought, "product");
+  return (
+    <div>
+      {productBought?.productBought[0]?.productBought.map((res, i) => {
+        return (
+          <div key={i}>
+            <ReviewCard data={res} email={email} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Rating;
