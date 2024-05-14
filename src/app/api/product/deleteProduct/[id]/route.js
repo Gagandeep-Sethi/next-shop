9e202f910cb9acd65d1a8c44e16f237bccde134a
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-15 00:03:43 +0530

    dlt product

--- src/app/api/product/deleteProduct/[id]/route.js
+++ src/app/api/product/deleteProduct/[id]/route.js
@@ -10,11 +10,10 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-export async function DELETE(req, { params }) {
+export async function GET(req, { params }) {
   try {
     const product = await Product.findById(params.id);
     if (!product) {
-      console.log("product not found");
       return NextResponse.json(
         { message: "Product not found" },
         { status: 400 }
@@ -32,6 +31,7 @@ export async function DELETE(req, { params }) {
       }
     }
     await Product.findByIdAndDelete(params.id);
+
     return NextResponse.json({ message: "Product deleted " }, { status: 200 });
   } catch (error) {
     console.log(error);
@@ -42,7 +42,6 @@ async function deleteImages(images) {
   for (const publicId of images) {
     try {
       const result = await cloudinary.uploader.destroy(publicId);
-      console.log("Image deleted successfully:", result);
     } catch (error) {
       console.error("Error deleting image:", error);
       throw error;
